d53fce97d06c0e9e26c54b9d1a42d3455999defd
Withlaw <car2559@naver.com>
2024-03-05 00:00:13 +0900

    chore: cypress, 기타 패키지 설치 & cypress 설정 & esliint 설정

---
+++ cypress/e2e/routes.cy.ts
@@ -0,0 +1,5 @@
+it('displays correct heading when navigating to shows route', () => {
+	cy.visit('/main');
+	cy.findByRole('button', { name: '로그인' }).click();
+	cy.findByRole('heading', { name: '로그인' }).should('exist');
+});
