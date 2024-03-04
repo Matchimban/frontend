3b3d9d6223568107b7f849e74c7942fa8512bf25
Withlaw <car2559@naver.com>
2024-03-04 20:18:39 +0900

    feat: 회원가입 구현

--- app/(routes)/(main)/main/actions/siginout.action.ts
+++ app/(routes)/(main)/main/actions/siginout.action.ts
@@ -5,9 +5,7 @@ import { cookies } from 'next/headers';
 import { signOut } from '@/auth.ts';
 
 export const signout = async () => {
-	await signOut({
-		redirect: false,
-	});
 	cookies().delete('accessToken');
 	cookies().delete('refreshToken');
+	await signOut();
 };
