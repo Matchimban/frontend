b417f6263c83096ea2066d942654781c192aefe0
Withlaw <car2559@naver.com>
2024-03-05 11:09:21 +0900

    fix: url 환경 변수 설정, logout 헤더 수정

--- app/(routes)/(main)/main/actions/signin.action.ts
+++ app/(routes)/(main)/main/actions/signin.action.ts
@@ -1,30 +1,36 @@
 'use server';
 
-import { AuthError } from 'next-auth';
-
+// import { AuthError } from 'next-auth';
 import { signIn } from '@/auth.ts';
 
 export const signin = async (formdata: FormData) => {
-	try {
-		await signIn('credentials', formdata);
-		// *** NEXT_REDIRECT 오류 원인 찾기 ***
-		// const redirectUrl = await signIn('credentials', {
-		// 	...formdata,
-		// 	redirect: false,
-		// });
-	} catch (error) {
-		if (error instanceof Error) {
-			const { type, cause } = error as AuthError;
-			switch (type) {
-				case 'CredentialsSignin':
-					return 'Invalid credentials.';
-				case 'CallbackRouteError':
-					return cause?.err?.toString();
-				default:
-					return error.message;
-			}
-		}
+	// try {
+	await signIn('credentials', {
+		...formdata,
+		redirect: false,
+	});
+	// *** NEXT_REDIRECT 오류 원인 찾기 ***
+	// const redirectUrl = await signIn('credentials', {
+	// 	...formdata,
+	// 	redirect: false,
+	// });
+	// } catch (error) {
+	// if (isRedirectError(error)) {
+	// 	console.error('SignIn redirect error!');
+	// }
 
-		throw error;
-	}
+	// if (error instanceof Error) {
+	// 	const { type, cause } = error as AuthError;
+	// 	switch (type) {
+	// 		case 'CredentialsSignin':
+	// 			return 'Invalid credentials.';
+	// 		case 'CallbackRouteError':
+	// 			return cause?.err?.toString();
+	// 		default:
+	// 			return error.message;
+	// 	}
+	// }
+
+	// throw error;
+	// }
 };
