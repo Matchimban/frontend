8b0ee0f3079641172d72c7d13d4fcf2c5ff3df32
Withlaw <car2559@naver.com>
2024-03-06 21:05:28 +0900

    chore: next image 기본 설정

--- app/features/board/contents.component.tsx
+++ app/features/board/contents.component.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import type { Restaurant } from '@/app/features/board/_types.ts';
@@ -12,11 +13,13 @@ export default function Contents({ restaurants }: Props) {
 				href={`/main/restaurants/${restaurant.id}`}
 				className="flex w-full space-x-4 p-2"
 			>
-				<div className="flex-initial">
-					<figure className="h-32 w-28 bg-stone-300">
-						{/* <span>image</span> */}
-						<img src={restaurant.images} />
-					</figure>
+				<div className="w-40 flex-initial">
+					<Image
+						src={restaurant.images}
+						width={200}
+						height={100}
+						alt="restaurant thumnail"
+					/>
 				</div>
 				<div className="flex flex-auto justify-between">
 					<div className="flex flex-col">
