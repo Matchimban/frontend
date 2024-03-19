9f53de42b73df0308a08fa4a2a1bcc8fbf1d6799
Withlaw <car2559@naver.com>
2024-03-19 18:47:16 +0900

    test: 로그인 e2e 테스트 코드 작성 및 useRouter 모킹 추가

--- app/__tests__/restaurants/register-form.test.tsx
+++ app/__tests__/restaurants/register-form.test.tsx
@@ -3,6 +3,20 @@ import { render, screen } from '@testing-library/react';
 import RegisterForm from '@/app/features/restaurant/register-form.component';
 
 describe('매장 등록 폼 테스트', () => {
+	vi.mock('next/navigation', () => {
+		const actual = vi.importActual('next/navigation');
+		return {
+			...actual,
+			useRouter: vi.fn(() => ({
+				push: vi.fn(),
+			})),
+			useSearchParams: vi.fn(() => ({
+				get: vi.fn(),
+			})),
+			usePathname: vi.fn(),
+		};
+	});
+
 	test('상호명, 전화번호, 사업자번호, 주소, 카테고리, 영업시간, 휴일, 소개, 안내사항, 사진 input & 등록하기 버튼 렌더링', async () => {
 		render(<RegisterForm />);
 
@@ -10,7 +24,7 @@ describe('매장 등록 폼 테스트', () => {
 		expect(screen.getByLabelText('전화 번호')).toBeInTheDocument();
 		expect(screen.getByLabelText('사업자 번호')).toBeInTheDocument();
 		expect(screen.getByLabelText('주소')).toBeInTheDocument();
-		expect(screen.getByLabelText('카테고리')).toBeInTheDocument();
+		expect(screen.getByLabelText('분류')).toBeInTheDocument();
 		expect(screen.getByLabelText('영업 시간')).toBeInTheDocument();
 		expect(screen.getByLabelText('휴일')).toBeInTheDocument();
 		expect(screen.getByLabelText('소개')).toBeInTheDocument();
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -27,13 +27,35 @@ import '@testing-library/cypress/add-commands';
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add('signIn', (email, password) => {
+	// 로그인 버튼 클릭
+	cy.findByRole('button', { name: '로그인' }).click();
+
+	// 로그인 폼 작성
+	cy.findByPlaceholderText(/email/i).clear();
+	cy.findByPlaceholderText(/email/i).type(email);
+
+	cy.findByPlaceholderText(/password/i).clear();
+	cy.findByPlaceholderText(/password/i).type(password);
+
+	// 로그인 폼 제출
+	cy.findByRole('dialog').within(() => {
+		cy.findByRole('button', { name: '로그인' }).click();
+	});
+
+	// 로그인 버튼 대신 유저 이름 표시
+	cy.findByRole('button', { name: Cypress.env('USER_NAME') }).should('exist');
+});
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace Cypress {
+		interface Chainable {
+			signIn(email: string, password: string): Chainable<void>;
+			// drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+			// dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+			// visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+		}
+	}
+}
