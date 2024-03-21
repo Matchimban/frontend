34ae01a9cd07616f43457c190b57eb1e48802559
Withlaw <car2559@naver.com>
2024-03-21 13:18:39 +0900

    design: register 페이지를 restaurant 하위로 이동

--- app/(routes)/(main)/register/page.tsx
+++ app/(routes)/(main)/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from '@/app/features/restaurant/register-form.component';
 
 export default async function Page() {
 	return (
-		<div className="flex w-full max-w-xl justify-center">
+		<div className="flex min-h-svh w-full max-w-xl justify-center ">
 			<section className="w-full max-w-lg border bg-slate-50">
 				<div className="flex items-center bg-slate-200 px-4 py-2">
 					<h2 className="text-lg">매장 등록</h2>
