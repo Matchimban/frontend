f8313ee20dd4810038792a9729412850ba6d1270
Withlaw <car2559@naver.com>
2024-03-24 18:23:42 +0900

    fix: merge conflict 해결 (#8)
    * Update README.md
    
    * deploy test (#6)
    
    * chore: jest-dom&eslint-plugins 설치 / vitest 환경 설정 / PR template 생성 (#1)
    
    * Update README.md
    
    * chore: jest-dom&eslint-plugins 설치 및 vitest 환경 설정
    
    * docs: pull_request_template.md 생성
    
    * chore: recoil, antd 전역 설정
    
    * chore: clsx, msw, axios, react-query&devtools 패키지 설치
    
    * design: main header & nav ui layout
    
    * design: main header & nav responsive layout
    
    * chore: recoil, antd 전역 설정 & clsx, msw, axios, react-query&devtools 패키지 설치 (#2)
    
    * chore: recoil, antd 전역 설정
    
    * chore: clsx, msw, axios, react-query&devtools 패키지 설치
    
    * design: restaurant page layout
    
    * design: restaurant register page layout
    
    * chore: NextAuth.js 설치 및 기본설정
    
    * design: 로그인/회원가입 ui
    
    * feat: login 인증 구현
    
    * design: logout dropdown ui
    
    * feat: 회원가입 구현
    
    * feat: 미들웨어로 user route 보호 기능 구현
    
    * feat: 로그인 에러 피드백 & 로그아웃
    
    * chore: cypress, 기타 패키지 설치 & cypress 설정 & esliint 설정
    
    * fix: url 환경 변수 설정, logout 헤더 수정
    
    * chore: next image 기본 설정
    
    * feat: login 버튼 ssr에서 csr로 변경
    
    * user-event lib 설치, main 라우트 수정
    
    * feat: login error feedback 추가
    
    * test: login 테스트 코드 작성
    
    * signin action 에러 핸들링 수정 및 회원가입 test 작성
    
    * design: 매장 등록 ui
    
    * feat: 이미지 등록 및 미리보기 구현
    
    * feat: 인증 토큰 갱신 및 자동 로그아웃 구현
    
    * design: 매장 전체, 상세 페이지 ui
    
    * feat: protect page 접근 권한 설정
    
    * feat: 로그인/회원가입 폼 lazy laoding 처리
    
    * test: register form 테스트 코드 작성
    
    * fix: 토큰 만료시간 버그 수정
    
    * feat: 매장 등록 및 조회 기능 구현
    
    * design: 매장 등록 폼 유효성 검사 조건 및 버튼 ui 수정
    
    * refactor: api service 로직 분리
    
    * test: 로그인/회원가입 모달 cypress 테스트 코드 작성
    
    * design: 매장 등록 폼 수정 및 에러 피드백 추가
    
    * design: 매장 상세페이지 ui 수정
    
    * test: 로그인 e2e 테스트 코드 작성 및 useRouter 모킹 추가
    
    * feat: 회원가입 에러 피드백 추가
    
    * chore: 배포 테스트 (#5)
    
    * design: register 페이지를 restaurant 하위로 이동
    
    * chore: github actions 설정(vercel 배포)
    
    * merge

--- app/constants/path.ts
+++ app/constants/path.ts
@@ -1,3 +1,3 @@
 export const baseUrl = process.env.BASE_URL;
 
-export const PROTECTED_PATH = ['/register', '/restaurant/edit'];
+export const PROTECTED_PATH = ['/restaurant/register', '/restaurant/edit'];
